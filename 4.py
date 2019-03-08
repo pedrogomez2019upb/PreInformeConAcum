193404aa718170f63f238e32ffaa3ddbf5e467bd
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-07 21:47:04 -0500

    Se añade el trabajo 4.py funcionando y compilando.

---
+++ 4.py
@@ -0,0 +1,8 @@
+#Algoritmo que hace cuenta regresiva de 100 a 1 en 3
+#Primero defiinimos el contador desde 0
+contador_regresivo = 0
+#Ahora ponemos una condicional para que cuente de cien a cero restandole -3 
+#cada vez que vaya contando
+for x in range (100,0,-3):
+    print("La cuenta regresiva es: {}".format(x))
+#Desarrollado por Pedro Gómez / ID:000396221
