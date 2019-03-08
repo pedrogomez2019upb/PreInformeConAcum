aec68ca68993f6f06a680cce9d14625cc5967d19
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-07 21:35:26 -0500

    Se creo el archivo 4.py para empezar el desarrollo del punto #4 del taller

---
+++ 3.py
@@ -0,0 +1,7 @@
+#Algoritmo que lea 5 números y lea el mayor de ellos
+#Primero vamos a poner una condicional for para que los lea en el rango de 0 y 5
+for x in range(0,5):
+    #Se crea la variable que va a recibir los valores a evaular.
+    num = float(input("Por favor ingrese el número a evaluar: "))
+    #Ponemos un if y then para que revise la cantidad de números
+    
