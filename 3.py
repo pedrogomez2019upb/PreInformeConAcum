f34a16ee637fc09b5e4bf59f0fec1f8c9c73ba94
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-08 07:50:15 -0500

    Se añade el archivo 3.py funcionando y compilando

--- 3.py
+++ 3.py
@@ -1,7 +1,14 @@
 #Algoritmo que lea 5 números y lea el mayor de ellos
 #Primero vamos a poner una condicional for para que los lea en el rango de 0 y 5
-for x in range(0,5):
+for i in range(0,5):
     #Se crea la variable que va a recibir los valores a evaular.
-    num = float(input("Por favor ingrese el número a evaluar: "))
+    x = float(input("Por favor ingrese el número a evaluar: "))
     #Ponemos un if y then para que revise la cantidad de números
+    if i ==0:
+        mayor=x
+    else:
+        if x>mayor:
+            mayor=x
+print("El número mayor es: {} ".format(mayor))
+#Desarrollado por Pedro Gómez / ID:000396221
     
